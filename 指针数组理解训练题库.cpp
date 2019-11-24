79e37a822e513a20501ec63fabcd316889c55a1d
杜欣宇 <2331102566@qq.com>
2019-11-24 22:26:20 +0800

    指针、数组理解训练题库

---
+++ 指针数组理解训练题库.cpp
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <windows.h>
+#include <string.h>
+
+void Example_one()
+{
+	int a[] = { 1,2,3,4 };
+	printf("%d\n", sizeof(a));
+	printf("%d\n", sizeof(a+0));
+	printf("%d\n", sizeof(*a));
+	printf("%d\n", sizeof(a+1));
+	printf("%d\n", sizeof(a[1]));
+	printf("%d\n", sizeof(&a));
+	printf("%d\n", sizeof(*&a)); 
+	printf("%d\n", sizeof(&a+1));
+	printf("%d\n", sizeof(&a[0]));
+	printf("%d\n", sizeof(&a[0]+1));
+}
+
+void Example_two()
+{
+	char arr[] = { 'a','b','e','d','f','g'};
+	printf("%d\n", sizeof(arr));
+	printf("%d\n", sizeof(arr + 0));
+	printf("%d\n", sizeof(*arr));
+	printf("%d\n", sizeof(arr[1]));
+	printf("%d\n", sizeof(&arr));
+	printf("%d\n", sizeof(&arr + 1));
+	printf("%d\n", sizeof(&arr[0] + 1));
+
+	printf("%d\n", strlen(arr));
+	printf("%d\n", strlen(arr + 0));
+	//有报错，所以说明这些写法不合法
+	//printf("%d\n", strlen(*arr));
+	//printf("%d\n", strlen(arr[1]));
+	//printf("%d\n", strlen(&arr));
+	//printf("%d\n", strlen(&arr + 1));
+	printf("%d\n", strlen(&arr[0] + 1));
+}
+
+void Example_three()
+{
+	char arr[] = "abcdef";
+	printf("%d\n", sizeof(arr));
+	printf("%d\n", sizeof(arr + 0));
+	printf("%d\n", sizeof(*arr));
+	printf("%d\n", sizeof(arr[1]));
+	printf("%d\n", sizeof(&arr));
+	printf("%d\n", sizeof(&arr + 1));
+	printf("%d\n", sizeof(&arr[0] + 1));
+
+	printf("%d\n", strlen(arr));
+	printf("%d\n", strlen(arr + 0));
+	//有报错，所以说明这些写法不合法
+	//printf("%d\n", strlen(*arr));
+	//printf("%d\n", strlen(arr[1]));
+	//printf("%d\n", strlen(&arr));
+	//printf("%d\n", strlen(&arr + 1));
+	printf("%d\n", strlen(&arr[0] + 1));
+}
+
+void Example_four()
+{
+	const char *p = "abcdef";
+	printf("%d\n", sizeof(p));
+	printf("%d\n", sizeof(p + 1));
+	printf("%d\n", sizeof(*p));
+	printf("%d\n", sizeof(p[1]));
+	printf("%d\n", sizeof(&p));
+	printf("%d\n", sizeof(&p + 1));
+	printf("%d\n", sizeof(&p[0] + 1));
+
+	printf("%d\n", strlen(p));
+	printf("%d\n", strlen(p + 1));
+	//有报错，所以说明这些写法不合法
+	//printf("%d\n", strlen(*p));
+	//printf("%d\n", strlen(p[0]));
+	//printf("%d\n", strlen(&p));
+	//printf("%d\n", strlen(&p + 1));
+	printf("%d\n", strlen(&p[0] + 1));
+}
+
+void Example_five()
+{
+	int a[3][4] = {0};
+	printf("%d\n", sizeof(a));
+	printf("%d\n", sizeof(a[0][0]));
+	printf("%d\n", sizeof(a[0]));
+	printf("%d\n", sizeof(a[0] + 1));
+	printf("%d\n", sizeof(*(a[0]+1)));
+	printf("%d\n", sizeof(a+1));
+	printf("%d\n", sizeof(*(a+1)));
+	printf("%d\n", sizeof(&a[0] + 1));
+	printf("%d\n", sizeof(*(&a[0] + 1)));
+	printf("%d\n", sizeof(*a));
+	printf("%d\n", sizeof(a[3]));
+	
+}
+
+int main()
+{
+	Example_one();
+	Example_two();
+	Example_three();
+	Example_four();
+	Example_five();
+	system("pause");
+	return 0;
+}
