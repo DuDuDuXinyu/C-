9082844c1edaef7f26933c7460b03b845042cebd
杜欣宇 <2331102566@qq.com>
2020-03-08 11:16:48 +0800

    LeetCode--List(2).c

--- LeetCode--List(2).c
+++ LeetCode--List(2).c
@@ -69,3 +69,53 @@ struct ListNode* partition(struct ListNode* head, int x)
 
 	return lessNode.next;
 }
+
+//找到带环链表的环起点
+
+struct ListNode* detectCycle(struct ListNode* head) {
+	Node* fast = head;
+	Node* low = head;
+
+	//通过快慢指针来判断链表是否带环
+	//isCircycle则是用来表示链表是否带环的标志
+
+	bool isCircycle = false;
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		low = low->next;
+		if (low == fast)
+		{
+			isCircycle = true;
+			break;
+		}
+	}
+
+	//不带环，直接返回NULL
+
+	if (!isCircycle)
+	{
+		return NULL;
+	}
+
+	//fast为快慢指针相遇的第一次相遇点
+	//由于快慢指针的特性可以得知：当快慢指针相遇时，慢指针并未走完一圈
+	//此时让head从头开始，fast和head同时以一次一步的开始遍历
+	//当fast等于head的时候就是环的起点
+	//head到环的起点距离为L
+	//环的起点到fast的距离为X
+	//环长为R
+	//fast距离环起点的距离为R-X
+	//由fast和low的关系可知：2L+2X=L+X+nR(前面的2倍的low的路程，后面是fast的路程)
+	//L=nR-X->{n=1,L=R-X}{n=2,L=R-X+R}
+	//可以得知不管n为多少，当head走完L时，fast也刚好走完nR-X
+	//则得出head==fast时是环的起点
+
+	while (fast != head)
+	{
+		fast = fast->next;
+		head = head->next;
+	}
+
+	return head;
+}
